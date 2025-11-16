a7f67fa45c2ec6438b8600bd0e4a1336ce43962f
missou <695383649@qq.com>
2025-11-16 17:27:48 +0800

    优化lib打包配置

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -11,12 +11,11 @@ export default defineConfig({
     lib: {
       name: 'lib',
       entry: {
-        'vue-compiler-sfc': './node_modules/vue/compiler-sfc/index.browser.mjs',
         vue: './node_modules/vue/dist/vue.esm-browser.js',
         'naive-ui': './node_modules/naive-ui/dist/index.mjs',
         echarts: './node_modules/echarts/index.js',
-        'antv-g6': './node_modules/@antv/g6/esm/index.js',
-        'antv-g6-v4': './node_modules/@antv/g6-v4/es/index.js',
+        '@antv/g6': './node_modules/@antv/g6/esm/index.js',
+        '@antv/g6-v4': './node_modules/@antv/g6-v4/es/index.js',
       },
       formats: ['es'],
     },
